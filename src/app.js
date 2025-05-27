ec57e9079d5be85b46523bfb1a6bc88b0164b69e
Cristian <gustavo@MacBook-Pro.local>
2025-05-27 01:05:18 -0500

    cambios

--- src/app.js
+++ src/app.js
@@ -8,8 +8,7 @@ const conectMongo = require('./config/mongo');
 conectMongo();
 app.use(cors({
   origin: ['http://localhost:5173', 'https://TU-FRONT.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(express.json());
 
